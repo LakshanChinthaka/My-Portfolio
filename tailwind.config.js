fdd9785bd78ee015226da40e8fd6802603c25cc2
Lakshan Chinthaka <chinthaka21022@gmail.com>
2024-03-27 10:08:53 +0530

    fix issue v.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    // './node_modules/tw-elements/dist/js/**/*.{js,ts,jsx,tsx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   // darkMode: 'class',
   enabled: process.env.NODE_ENV === "production",
@@ -15,9 +15,6 @@ module.exports = {
       }
     },
 
-    // plugins: [
-    //   require('tw-elements/dist/plugin')
-  // ],
 
     fontFamily: {
       'body': [
